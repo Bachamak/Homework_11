798c6f973f626cc6ab0582d2bcdf9e383c74737e
Bachamak <tarasenkodaniilromanovich@gmail.com>
2023-02-20 16:54:54 +0200

    Подкорректировано

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let amount = prompt("Введите количество элементов");
+let amount = prompt("Введите количество элементов через запятую");
 let arr = amount.split(',');
 console.log(arr);
 
 let sortnumbers = arr.sort((a,b) => a - b);
 console.log(sortnumbers);
 
-sortnumbers.splice(2, 3);
+sortnumbers.splice(1, 3);
 console.log(sortnumbers);
 // 3, -9, 8, 38, -4, 0
